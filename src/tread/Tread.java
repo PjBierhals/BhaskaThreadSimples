4a64a0445fa31486ee1e6686d9e10299bc67d422
PjBierhals <pjbierhals@gmail.com>
2022-02-09 00:01:35 -0300

    Initial commit

---
+++ src/tread/Tread.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tread;
+
+/**
+ *
+ * @author pjbie
+ */
+public class Tread {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+       ResulThread resultados = new ResulThread(2, 12, -14);
+      
+
+        resultados.start();
+      
+       
+        
+       
+       
+        
+       // System.out.println("Fim!");
+    }
+
+}
